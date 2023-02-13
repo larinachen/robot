b7036084ec462450435feeccb189d09a49e6606a
Larina Chen <larinachen2024@u.northwestern.edu>
2023-02-13 12:26:20 -0600

    finish part b simulation

--- simulation.py
+++ simulation.py
@@ -1,2 +1,16 @@
-bullet3
-venv
+import pybullet as p
+import time
+
+# create physics object
+physicsClient = p.connect(p.GUI)
+# pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_GUI,0)
+
+# stepping throuhgh the world using 1000 steps
+
+for i in range(1000):
+	print(f"starting iteration {i}")
+	p.stepSimulation()
+	time.sleep(0.01)
+
+
+p.disconnect()
